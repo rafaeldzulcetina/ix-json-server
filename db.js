7f13fef14f93bdeea5116183057b8e5ca05b4a72
Benjamín Martínez <benjamin.martinez@xid.com.mx>
2022-06-14 09:26:30 -0500

    se añaden rutas y datos de tours

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const db_invitations = require('./jsons/db_invitations.json');
 const db_salesRooms = require('./jsons/db_salesroom.json');
+const db_tours = require('./jsons/db_tours.json');
 
 module.exports = function() {
     return Object.assign({},
         db_invitations,
-        db_salesRooms
+        db_salesRooms,
+        db_tours
     )
 }
