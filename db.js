4e4f5d44a33cf392bf0ef334adb00da339a81698
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-06-28 14:54:05 -0500

    feature: agregando catalogo de identificaciones

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const db_invitations = require('./jsons/db_invitations.json');
 const db_salesRooms = require('./jsons/db_salesroom.json');
 const db_tours = require('./jsons/db_tours.json');
+const db_ids = require('./jsons/db_ids.json');
 
-module.exports = function() {
+module.exports = function () {
     return Object.assign({},
         db_invitations,
         db_salesRooms,
-        db_tours
+        db_tours,
+        db_ids,
     )
 }
