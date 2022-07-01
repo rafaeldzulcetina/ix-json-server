f17b08cd2d32635b1c5cdc550e4e7be3b12c1408
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-07-01 10:30:20 -0500

    feat: added contract status cat

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const db_invitations = require('./jsons/db_invitations.json');
 const db_salesRooms = require('./jsons/db_salesroom.json');
 const db_tours = require('./jsons/db_tours.json');
 const db_ids = require('./jsons/db_ids.json');
+const contract_status = require('./jsons/contract_status.json');
 
 module.exports = function () {
     return Object.assign({},
@@ -9,5 +10,6 @@ module.exports = function () {
         db_salesRooms,
         db_tours,
         db_ids,
+        contract_status
     )
 }
