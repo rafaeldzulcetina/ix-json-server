4d4a96556a48591263e92ed4a13a51abde273fcc
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-07-04 10:37:10 -0500

    Contract Roles

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const db_salesRooms = require('./jsons/db_salesroom.json');
 const db_tours = require('./jsons/db_tours.json');
 const db_ids = require('./jsons/db_ids.json');
 const contract_status = require('./jsons/contract_status.json');
+const contract_roles = require('./jsons/contract_roles.json');
 
 module.exports = function () {
     return Object.assign({},
@@ -10,6 +11,7 @@ module.exports = function () {
         db_salesRooms,
         db_tours,
         db_ids,
-        contract_status
+        contract_status,
+        contract_roles
     )
 }
